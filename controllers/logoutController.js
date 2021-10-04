4a32a12c1207723f4e9539afea35485a1a2b073a
gitdagray <DaveOnEleven@gmail.com>
2021-10-03 19:48:22 -0500

    maxAge option not required when deleting cookie

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -15,7 +15,7 @@ const handleLogout = async (req, res) => {
     // Is refreshToken in db?
     const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
     if (!foundUser) {
-        res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.clearCookie('jwt', { httpOnly: true });
         return res.sendStatus(204);
     }
 
@@ -28,7 +28,7 @@ const handleLogout = async (req, res) => {
         JSON.stringify(usersDB.users)
     );
 
-    res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // secure: true - only serves on https
+    res.clearCookie('jwt', { httpOnly: true }); // secure: true - only serves on https
     res.sendStatus(204);
 }
 
